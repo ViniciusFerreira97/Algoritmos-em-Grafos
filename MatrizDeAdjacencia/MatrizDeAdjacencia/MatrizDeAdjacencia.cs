049572de7e8886f7995431145f5aff4d128b6d46
Vinicius De Jesus Ferreira <1065441@sga.pucminas.br>
2018-04-20 19:27:02 -0300

    Implementação da classe principal

---
+++ MatrizDeAdjacencia/MatrizDeAdjacencia/MatrizDeAdjacencia.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizDeAdjacencia
+{
+    class MatrizDeAdjacencia
+    {
+        private int[,] MA;
+        public MatrizDeAdjacencia(int x)
+        {
+            MA = new int[x, x];
+            for(int i = 0; i<x;i++)
+            {
+                for(int j = 0; j<x; j++)
+                {
+                    MA[i, j] = 0;
+                }
+            }
+        }
+        private int grauVertice(int v)
+        {
+            int grau = 0;
+            for(int i=0; i<MA.GetLength(1); i++)
+            {
+                if (MA[v, i] == 1)
+                    grau++;
+            }
+            return grau;
+        }
+        private void inserirArestas(int x, int y)
+        {
+            MA[x, y] = 1;
+            MA[y, x] = 1;
+        }
+        private void removerArestas(int x, int y)
+        {
+            MA[x, y] = 0;
+            MA[y, x] = 0;
+        }
+        private bool completo()
+        {
+            int aux = MA.GetLength(0);
+            int qtdArestas = 0;
+            for (int i = 0; i < MA.GetLength(0); i++)
+            {
+                for (int j = 0; j < MA.GetLength(1); j++)
+                {
+                    if(i != j)
+                    {
+                        if (MA[i, j] == 1)
+                            qtdArestas++;
+                    }
+                }
+            }
+            if (qtdArestas / 2 == aux)
+                return true;
+            else
+                return false;
+        }
+    }
+
+}
